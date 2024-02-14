cd320ce5f6f3f1e6281faeca567fbdc8c1e522de
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-02-14 21:53:51 +0800

    Update BlueTank.gd

--- Scenes/Enemies/BlueTank.gd
+++ Scenes/Enemies/BlueTank.gd
@@ -1,9 +1,9 @@
 extends PathFollow2D
 
-var speed = 5
+var speed = 150
 
 func _physics_process(delta):
 	move(delta)
 	
 func move(delta):
-	set_offset(get_offset() + speed + delta)
+	set_offset(get_offset() + speed * delta)
