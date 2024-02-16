cc7a618bbdcaf0fe8b478f1e6fb7cb387189382d
TenshinAkuma <hansclarkolino@sksu.edu.ph>
2024-02-16 21:38:30 +0800

    implement turret shooting and enemy destruction

--- Scenes/Turrets/Turrets.gd
+++ Scenes/Turrets/Turrets.gd
@@ -1,20 +1,30 @@
 extends Node2D
 
+var build_type
 var enemy_array = []
 var is_built = false
 var enemy
+var is_ready = true
 
 func _ready():
 	if is_built:
-		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[self.get_name()]["range"]
+		self.get_node("Range/CollisionShape2D").get_shape().radius = 0.5 * GameData.tower_data[build_type]["range"]
 		
 
 func _physics_process(delta):
 	if enemy_array.size() != 0 and is_built:
 		select_enemy()
 		turn()
+		if is_ready:
+			fire()
 	else:
 		enemy = null
+	
+func fire():
+	is_ready = false
+	enemy.on_hit(GameData.tower_data[build_type]["damage"])
+	yield(get_tree().create_timer(GameData.tower_data[build_type]["rof"]), "timeout")
+	is_ready = true
 	
 func turn():
 	get_node("Turret").look_at(enemy.position)
